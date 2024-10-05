6656d3cf5ea01ec73fde5f9e8a9545e41e4d8a9f
Izanalgit <izanalvarez@gmail.com>
2024-10-05 20:26:04 +0200

    feat : fetch DELETE hook

---
+++ src/hooks/useFetchDELETE.jsx
@@ -0,0 +1,43 @@
+import { useState , useEffect } from "react";
+import axios from "axios";
+
+//REVIEW
+
+const useFetchDELETE = (url,payload) => {
+    
+    const [data, setData] = useState(null);
+    const [status, setStatus] = useState(false);
+    const [errMsg, setErrMsg] = useState(null);
+   
+    useEffect(()=>{
+        if(payload)
+            axios.delete(url,{data:payload})
+                .then((response) => {
+                    setData(response);
+                })
+                .catch((error) => {
+                    if (error.response) {
+                        if(error.response.data.errors)
+                            setErrMsg(error.response.data.errors);
+                        else
+                            setErrMsg(`Error ${error.response.status} : ${error.response.data.message}`);
+                    } else if (error.request) {
+                        setErrMsg(`No responde`);
+                        console.log(error.request);
+                    } else {
+                        setErrMsg(`Error desconocido`);
+                        console.log(error.message);
+                    }
+                })
+                .finally(() => {
+                    setStatus(true);
+                })
+
+    },[url,payload]);
+
+
+    return {data,status,errMsg};
+
+}
+
+export default useFetchDELETE;
