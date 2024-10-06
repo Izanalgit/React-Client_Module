239be261740b06e00c64780c6d9b862dd57c4989
Izanalgit <izanalvarez@gmail.com>
2024-10-06 20:31:40 +0200

    feat : fetch PATCH hook

---
+++ src/hooks/useFetchPATCH.jsx
@@ -0,0 +1,43 @@
+import { useState , useEffect } from "react";
+import axios from "axios";
+
+const useFetchPATCH = (url,payload) => {
+    
+    const [data, setData] = useState(null);
+    const [status, setStatus] = useState(false);
+    const [errMsg, setErrMsg] = useState(null);
+   
+    //REVIEW
+
+    useEffect(()=>{
+        if(payload)
+            axios.patch(url,payload)
+                .then((response) => {
+                    setData(response);
+                })
+                .catch((error) => {
+                    if (error.response) {
+                        if(error.response.data.errors)
+                            setErrMsg(error.response.data.errors);
+                        else
+                            setErrMsg(`Error ${error.response.status} : ${error.response.data.message}`);
+                    } else if (error.request) {
+                        setErrMsg(`No responde`);
+                        console.log(error.request);
+                    } else {
+                        setErrMsg(`Error desconocido`);
+                        console.log(error.message);
+                    }
+                })
+                .finally(() => {
+                    setStatus(true);
+                })
+
+    },[url,payload]);
+
+
+    return {data,status,errMsg};
+
+}
+
+export default useFetchPATCH;
