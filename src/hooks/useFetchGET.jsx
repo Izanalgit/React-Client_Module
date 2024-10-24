1f899dda8dd3f6bc74d6385d8966332f261591d1
Izanalgit <izanalvarez@gmail.com>
2024-10-24 16:28:58 +0200

    fix : get fetch hook

--- src/hooks/useFetchGET.jsx
+++ src/hooks/useFetchGET.jsx
@@ -3,17 +3,17 @@ import axios from "axios";
 
 const useFetchGET = () => {
     const [data, setData] = useState(null);
-    const [loading, setLoading] = useState(true);
+    const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null); 
 
-    const fetchData = async (url,config) => {
-        // if (loading) return;
+    const fetchData = async (url, config) => {
         setLoading(true); 
         setError(null);
 
         try {
-            const response = await axios.get(url,config);
+            const response = await axios.get(url, config);
             setData(response.data);
+            return response.data; 
 
         } catch (error) {
             const errorMessage = error.response
@@ -21,12 +21,12 @@ const useFetchGET = () => {
                 : "El servidor no responde";
             
             setError(errorMessage);
+            return null;
 
         } finally {
             setLoading(false);
         }
     };
-
 
     return { fetchData, data, loading, error };
 };
