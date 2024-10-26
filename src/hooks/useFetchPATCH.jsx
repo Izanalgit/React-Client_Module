49a224f1db51a2e8480c5cf7e44714f700e76cc4
Izanalgit <izanalvarez@gmail.com>
2024-10-26 19:30:07 +0200

    fix : patch hook logics

--- src/hooks/useFetchPATCH.jsx
+++ src/hooks/useFetchPATCH.jsx
@@ -1,43 +1,37 @@
-import { useState , useEffect } from "react";
+import { useState } from "react";
 import axios from "axios";
 
-const useFetchPATCH = (url,payload) => {
-    
+const useFetchPATCH = () => {
     const [data, setData] = useState(null);
-    const [status, setStatus] = useState(false);
-    const [errMsg, setErrMsg] = useState(null);
-   
-    //REVIEW
+    const [headers, setHeaders] = useState(null); 
+    const [loading, setLoading] = useState(false); 
+    const [error, setError] = useState(null);
 
-    useEffect(()=>{
-        if(payload)
-            axios.patch(url,payload)
-                .then((response) => {
-                    setData(response);
-                })
-                .catch((error) => {
-                    if (error.response) {
-                        if(error.response.data.errors)
-                            setErrMsg(error.response.data.errors);
-                        else
-                            setErrMsg(`Error ${error.response.status} : ${error.response.data.message}`);
-                    } else if (error.request) {
-                        setErrMsg(`No responde`);
-                        console.log(error.request);
-                    } else {
-                        setErrMsg(`Error desconocido`);
-                        console.log(error.message);
-                    }
-                })
-                .finally(() => {
-                    setStatus(true);
-                })
+    const fetchData = async (url, payload, config = {}) => {
+        // if (loading) return;
+        setLoading(true); 
+        setError(null); 
+        
+        try {
+            const response = await axios.patch(url, payload, config);
 
-    },[url,payload]);
+            setData(response.data); 
+            setHeaders(response.headers); 
 
+        } catch (error) {
+            const errorMessage = error.response
+                ? `STATUS ${error.response.status} : ${
+                    error.response.data.messageErr ?
+                    error.response.data.messageErr:
+                    error.response.data.errors.map(error=>error.msg)}`
+                : "El servidor no responde";
+            setError(errorMessage);
+        } finally {
+            setLoading(false);
+        }
+    };
 
-    return {data,status,errMsg};
-
-}
+    return { fetchData, data, headers, loading, error };
+};
 
 export default useFetchPATCH;
