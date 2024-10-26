d2b528a9baf5b271cae78eb1999a2a155af752d9
Izanalgit <izanalvarez@gmail.com>
2024-10-26 19:30:34 +0200

    fix : delete hook logics

--- src/hooks/useFetchDELETE.jsx
+++ src/hooks/useFetchDELETE.jsx
@@ -1,43 +1,40 @@
-import { useState , useEffect } from "react";
+import { useState } from "react";
 import axios from "axios";
 
-//REVIEW
-
-const useFetchDELETE = (url,payload) => {
-    
+const useFetchDELETE = () => {
     const [data, setData] = useState(null);
-    const [status, setStatus] = useState(false);
-    const [errMsg, setErrMsg] = useState(null);
-   
-    useEffect(()=>{
-        if(payload)
-            axios.delete(url,{data:payload})
-                .then((response) => {
-                    setData(response);
-                })
-                .catch((error) => {
-                    if (error.response) {
-                        if(error.response.data.errors)
-                            setErrMsg(error.response.data.errors);
-                        else
-                            setErrMsg(`Error ${error.response.status} : ${error.response.data.message}`);
-                    } else if (error.request) {
-                        setErrMsg(`No responde`);
-                        console.log(error.request);
-                    } else {
-                        setErrMsg(`Error desconocido`);
-                        console.log(error.message);
-                    }
-                })
-                .finally(() => {
-                    setStatus(true);
-                })
+    const [headers, setHeaders] = useState(null); 
+    const [loading, setLoading] = useState(false); 
+    const [error, setError] = useState(null);
 
-    },[url,payload]);
+    const fetchData = async (url, payload = {}, config = {}) => {
+        // if (loading) return;
+        setLoading(true); 
+        setError(null); 
+        
+        try {
+            const response = await axios.delete(url, {
+                ...config,
+                data: payload,
+            });
 
+            setData(response.data); 
+            setHeaders(response.headers); 
 
-    return {data,status,errMsg};
+        } catch (error) {
+            const errorMessage = error.response
+                ? `STATUS ${error.response.status} : ${
+                    error.response.data.messageErr ?
+                    error.response.data.messageErr:
+                    error.response.data.errors.map(error=>error.msg)}`
+                : "El servidor no responde";
+            setError(errorMessage);
+        } finally {
+            setLoading(false);
+        }
+    };
 
-}
+    return { fetchData, data, headers, loading, error };
+};
 
 export default useFetchDELETE;
