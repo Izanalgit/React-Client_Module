8342a206c5d34cb2340e728bedb93305f8214114
Izanalgit <izanalvarez@gmail.com>
2024-10-26 19:31:09 +0200

    feat : dashboard page basics

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,7 +1,44 @@
+import { useState } from "react";
+
+import { useApp } from "../context/AppContext";
+import UserUpdateFrom from "../components/user/UserUpdateFrom";
+import UserDeleteForm from "../components/user/UserDeleteForm";
+
+
 const Dashboard = () => {
+
+    const [isEditingUser,setIsEditingUser] = useState(false);
+    const [isDeletingUser,setIsDeletingUser] = useState(false);
+    const [isManagingBlocks,setIsManagingBlocks] = useState(false);
+
+    const {logedIn} = useApp();
+
+    const handleCompleteAction = () => {
+        setIsEditingUser(false);
+        setIsManagingBlocks(false);
+    };
+
     return (
+        
         <>
-            <h1>DASHBORAD USUARIO AQUÍ !</h1>
+            {!logedIn && <h5>No estás conectado!</h5>}
+            {logedIn &&
+            <>
+                <h2>DASHBORAD</h2>
+                {!isEditingUser && !isDeletingUser && !isManagingBlocks &&
+                    <p>Hola {logedIn}</p>
+                }
+                {isEditingUser && <UserUpdateFrom onComplete={handleCompleteAction} />}
+                {isDeletingUser && <UserDeleteForm />}
+
+                <button onClick={() => setIsEditingUser(!isEditingUser)}>
+                    {isEditingUser ? 'Cancelar' : 'Editar Usuario'}
+                </button>
+                <button onClick={() => setIsDeletingUser(!isDeletingUser)}>
+                    {isDeletingUser ? 'Cancelar' : 'Eliminar Usuario'}
+                </button>
+            </>
+            }
         </>
     )
     
