74b84843d5d76130018da467a3df0a6d212f6682
Izanalgit <izanalvarez@gmail.com>
2024-10-27 20:30:13 +0100

    feat : contact page implementation

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -1,9 +1,28 @@
+import { useApp } from "../context/AppContext";
+import { useNavigate } from "react-router-dom";
+
+import ContactList from "../components/contacts/ContactsList";
+
 const Contacts = () => {
+
+    const navigate = useNavigate();
+    const {logedIn} = useApp();
+
+    const handleButton = () => {
+        navigate('/search');
+    }
+
     return (
-        <>
-            <h1>LISTA DE CONTACTOS AQUÍ !</h1>
-        </>
-    )
+    <>
+        {!logedIn && <h5>No estás conectado!</h5>}
+        {logedIn &&
+            <>
+                <h1>LISTA DE CONTACTOS</h1>
+                <ContactList />
+                <button onClick={handleButton}>Buscar</button>
+            </>
+        }
+    </>)
     
 
 }
