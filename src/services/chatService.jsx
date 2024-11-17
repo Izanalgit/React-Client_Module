784a985064ae04fb5759c08afd45826f19b2a782
Izanalgit <izanalvarez@gmail.com>
2024-11-17 20:50:27 +0100

    feat : chat services

--- src/services/chatService.jsx
+++ src/services/chatService.jsx
@@ -1,11 +1,11 @@
 import useFetchGET from '../hooks/useFetchGET';
 import useFetchPOST from '../hooks/useFetchPOST';
 
-const useChatService = (url,contactId) => {
+const useChatService = (url,authToken) => {
     const getMessagesFetch = useFetchGET();
     const sendMessagesFetch = useFetchPOST(); 
 
-    const getMessages = async (authToken) => {
+    const getMessages = async (contactId) => {
         const headers = { Authorization: `${authToken}` };
         const chatData = await getMessagesFetch.fetchData(`${url}/api/chat/read/${contactId}`, { headers });
         return { 
@@ -15,9 +15,9 @@ const useChatService = (url,contactId) => {
         };
     };
 
-    const sendMessage = async (authToken,newMessage) => {
+    const sendMessage = async (contactId,newMessage) => {
         const headers = { Authorization: `${authToken}` };
-        const payload = {payload:{recep:contactId,message:newMessage}};
+        const payload = {recep:contactId,message:newMessage};
         const messageData = await sendMessagesFetch.fetchData(`${url}/api/chat/send`,{payload} ,{headers});
         return { 
             data: messageData,
