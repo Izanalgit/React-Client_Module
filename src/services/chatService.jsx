924cb3bd04425c730e30f9bc39bab28c5c3702ad
Izanalgit <izanalvarez@gmail.com>
2024-11-19 10:29:42 +0100

    fix : direct fetch without generic hook for send message on services

--- src/services/chatService.jsx
+++ src/services/chatService.jsx
@@ -1,30 +1,36 @@
 import useFetchGET from '../hooks/useFetchGET';
-import useFetchPOST from '../hooks/useFetchPOST';
+import axios from 'axios';
 
 const useChatService = (url,authToken) => {
     const getMessagesFetch = useFetchGET();
-    const sendMessagesFetch = useFetchPOST(); 
 
     const getMessages = async (contactId) => {
         const headers = { Authorization: `${authToken}` };
         const chatData = await getMessagesFetch.fetchData(`${url}/api/chat/read/${contactId}`, { headers });
         return { 
-            data: chatData,
-            loading: getMessagesFetch.loading, 
-            error: getMessagesFetch.error 
+            data: chatData
         };
     };
 
     const sendMessage = async (contactId,newMessage) => {
         const headers = { Authorization: `${authToken}` };
         const payload = {recep:contactId,message:newMessage};
-        const messageData = await sendMessagesFetch.fetchData(`${url}/api/chat/send`,{payload} ,{headers});
-        return { 
-            data: messageData,
-            loading: sendMessagesFetch.loading, 
-            error: sendMessagesFetch.error 
-        };
+        try {
+            const response = await axios.post(`${url}/api/chat/send`, {payload}, { headers });
+            return { data: response.data, error: null };
+        } catch (error) {
+            const errorMessage = error.response
+                ? `STATUS ${error.response.status} : ${
+                      error.response.data.messageErr ||
+                      (error.response.data.errors
+                          ? error.response.data.errors.map((err) => err.msg).join(", ")
+                          : "Error desconocido al enviar mensaje ...")
+                  }`
+                : "El servidor no responde";
+            return { data: null, error: errorMessage };
+        }
     };
+    
 
 
     return {
