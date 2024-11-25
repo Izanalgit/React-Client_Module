71e65683af63b7932bd41e2ac2057068023131de
Izanalgit <izanalvarez@gmail.com>
2024-11-25 16:29:24 +0100

    feat : get premy info on service

---
+++ src/services/premyService.jsx
@@ -0,0 +1,22 @@
+import useFetchGET from '../hooks/useFetchGET';
+
+const usePremyService = (url) => {
+    const premyCount = useFetchGET();
+
+    const getPremyCount = async (authToken) => {
+        const headers = { Authorization: `${authToken}` };
+        const premyData = await premyCount.fetchData(`${url}/api/premy/count`, { headers });
+        return { 
+            data: premyData,
+            loading: premyData.loading, 
+            error: premyData.error 
+        };
+    };
+
+
+    return {
+        getPremyCount,
+    };
+};
+
+export default usePremyService;
