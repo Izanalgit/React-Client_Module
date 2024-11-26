5962a93008184d129c03d380732f4b069934b28a
Izanalgit <izanalvarez@gmail.com>
2024-11-26 17:47:21 +0100

    fix : new messge advice

--- src/components/advices/NewMessageAdvice.jsx
+++ src/components/advices/NewMessageAdvice.jsx
@@ -1,17 +1,21 @@
-const NewMessageAdvice = ({messages}) => {
-    
-    const unreadMessages = messages.count != undefined 
-        ? messages.count.length > 0 
-            ? messages.count.map(contact => contact.count).reduce((rec,cur)=>rec+cur)
-            : null 
-        : null;
+const NewMessageAdvice = ({ messages }) => {
+    let unreadMessages = null;
 
-    return (<>
-        {unreadMessages &&    
-            <h4>M {unreadMessages}</h4>
-        }
-    </>)
+    if (messages && Array.isArray(messages.count)) {
+        unreadMessages = messages.count.length > 0
+            ? messages.count
+                .map(contact => contact.count || 0) 
+                .reduce((rec, cur) => rec + cur, 0)
+            : null;
+    }
 
-}
+    return (
+        <>
+            {unreadMessages !== null && unreadMessages > 0 && (
+                <h4>M {unreadMessages}</h4>
+            )}
+        </>
+    );
+};
 
 export default NewMessageAdvice;
