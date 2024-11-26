036e04020aacf18c64a1097caf7d055b5c3d88d4
Izanalgit <izanalvarez@gmail.com>
2024-11-26 16:06:10 +0100

    feat : added unread render logics on contact list

--- src/components/contacts/ContactsList.jsx
+++ src/components/contacts/ContactsList.jsx
@@ -1,10 +1,12 @@
 import { useApp } from "../../context/AppContext";
+import { useChatContext } from "../../context/ChatContext";
 
 import ContactCard from "./ContactCard";
 
 const ContactList = () => {
 
     const {userContacts} = useApp();
+    const {unRead} = useChatContext();
 
     const contacts = userContacts?.contactsList?.contacts || [];
     const requests = userContacts?.contactsList?.requests || [];
@@ -13,14 +15,24 @@ const ContactList = () => {
         <div>
             <h2>Contactos</h2>
             {contacts.length > 0 ? (
-                contacts.map((contact) => (
-                    <ContactCard 
-                        key={contact.contactId} 
-                        contactId={contact.contactId} 
-                        contactName={contact.contactName}
-                        contactType="contact" 
-                    />
-                ))
+                contacts.map((contact) => {
+                    const unreadMessages = unRead.count
+
+                    const count = unreadMessages.length > 0 ?
+                        unreadMessages.find(
+                            (contactUnread) => contactUnread.sender === contact.contactId
+                        ) : 0;
+                    
+                    return (
+                        <ContactCard 
+                            key={contact.contactId} 
+                            contactId={contact.contactId} 
+                            contactName={contact.contactName}
+                            contactType="contact"
+                            countUnread = {count} 
+                        />
+                    )
+                })
             ) : (
                 <p>No tienes contactos aún.</p>
             )}
