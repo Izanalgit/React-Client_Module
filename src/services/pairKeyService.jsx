10e8107509e2447b8342eca7396c2d08a9ca5061
Izanalgit <izanalvarez@gmail.com>
2024-11-29 19:28:53 +0100

    update : pair key service

--- src/services/pairKeyService.jsx
+++ src/services/pairKeyService.jsx
@@ -1,39 +1,43 @@
 import crypto from 'crypto-browserify';
 import { Buffer } from 'buffer';
 
+const usePairKeyServices = () => {
 
-// Encrypt Message with PUBLIC Key
-function encryptMessageWithPublicKey(message, publicKey) {
-    
-    const bufferMessage = Buffer.from(message, 'utf8');
-    const encryptedMessage = crypto.publicEncrypt(publicKey, bufferMessage);
-    
-    return encryptedMessage.toString('base64');
+    // Encrypt Message with PUBLIC Key
+    function encryptMessageWithPublicKey(message, publicKey) {
+        
+        const bufferMessage = Buffer.from(message, 'utf8');
+        const encryptedMessage = crypto.publicEncrypt(publicKey, bufferMessage);
+        
+        return encryptedMessage.toString('base64');
+    }
+
+    // Decrypt PRIVATE Key with provisional pass
+    function decryptPrivateKey(encryptedPrivateKey, password, ivHex, salt) {
+
+        const encryptionKey = crypto.pbkdf2Sync(password, salt, 100000, 32, 'sha256');
+        const decipher = crypto.createDecipheriv('aes-256-cbc', encryptionKey, Buffer.from(ivHex, 'hex'));
+
+        let decryptedKey = decipher.update(encryptedPrivateKey, 'hex', 'utf8');
+        decryptedKey += decipher.final('utf8');
+
+        return decryptedKey;
+    }
+
+    // Decrypt Message with PRIVATE Key
+    function decryptMessageWithPrivateKey(encryptedMessage, privateKey) {
+        
+        const bufferMessage = Buffer.from(encryptedMessage, 'base64');
+        const decryptedMessage = crypto.privateDecrypt(privateKey, bufferMessage);
+        
+        return decryptedMessage.toString('utf8');
+    }
+
+    return {
+        encryptMessageWithPublicKey,
+        decryptPrivateKey,
+        decryptMessageWithPrivateKey
+    }
 }
 
-// Decrypt PRIVATE Key with provisional pass
-function decryptPrivateKey(encryptedPrivateKey, password, ivHex, salt) {
-    
-    const encryptionKey = crypto.pbkdf2Sync(password, salt, 100000, 32, 'sha256');
-    const decipher = crypto.createDecipheriv('aes-256-cbc', encryptionKey, Buffer.from(ivHex, 'hex'));
-
-    let decryptedKey = decipher.update(encryptedPrivateKey, 'hex', 'utf8');
-    decryptedKey += decipher.final('utf8');
-
-    return decryptedKey;
-}
-
-// Decrypt Message with PRIVATE Key
-function decryptMessageWithPrivateKey(encryptedMessage, privateKey) {
-    
-    const bufferMessage = Buffer.from(encryptedMessage, 'base64');
-    const decryptedMessage = crypto.privateDecrypt(privateKey, bufferMessage);
-    
-    return decryptedMessage.toString('utf8');
-}
-
-export {
-    encryptMessageWithPublicKey,
-    decryptPrivateKey,
-    decryptMessageWithPrivateKey
-}
+export default usePairKeyServices;
