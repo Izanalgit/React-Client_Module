b292d458b9048e06a444ca8a29769fb963b49bde
Izanalgit <izanalvarez@gmail.com>
2024-11-29 19:27:21 +0100

    feat : init buffer and proces on main

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,20 @@ import App from './App.jsx'
 import { AppProvaider } from './context/AppContext.jsx'
 import './css/index.css'
 
+import { Buffer } from 'buffer';
+import process from 'process';
+
+if (!window.Buffer) 
+    window.Buffer = Buffer;
+
+if (!window.process)
+    window.process = process;
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <AppProvaider>
-      <App />
-    </AppProvaider>
-  </React.StrictMode>,
+    <React.StrictMode>
+        <AppProvaider>
+            <App />
+        </AppProvaider>
+    </React.StrictMode>,
 )
