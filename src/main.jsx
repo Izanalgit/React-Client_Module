29d523f65e06bf07f9cb5e4420111f170045d3e3
Izanalgit <izanalvarez@gmail.com>
2024-12-04 13:23:37 +0100

    fix : with inited process on main probe

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { AppProvaider } from './context/AppContext.jsx'
 import './css/index.css'
-
+import process from 'process'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     // <React.StrictMode>
