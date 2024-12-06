fc2c1da48eb32d3b19260e6ce3eda68aa14b6a53
Izanalgit <izanalvarez@gmail.com>
2024-12-06 21:09:17 +0100

    update : navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,4 +1,4 @@
-import {Link} from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 import { useApp } from '../context/AppContext';
 import { useChatContext } from '../context/ChatContext';
@@ -6,30 +6,34 @@ import { useChatContext } from '../context/ChatContext';
 import NewContactAdvice from './advices/NewContactAdvice';
 import NewMessageAdvice from './advices/NewMessageAdvice';
 
-// import '../css/NavBar.css'  ->   className={'butNav glow'}
+import '../css/NavBar.css' 
+//  ->   className={'butNav glow'}
 
 const NavBar = () => {
 
     const {userContacts , logedIn} = useApp();
     const {unRead} = useChatContext();
 
-    return(<>
-        <nav>
-            <Link to="/" >Inicio - </Link>
-            <Link to="/dashboard" >Dashboard - </Link>
-            <Link to="/profile" >Perfil - </Link>
-            <Link to="/contacts" >Contactos - </Link>
-            <Link to="/search" >Buscar - </Link>
-            <Link to="/login" >CONECTAR / </Link>
-            <Link to="/logout" >DESCONTECTAR</Link>
-        </nav>
+    return(<div className='navegation'>
         {logedIn &&
-            <>
+            <div className='warningsDiv'>
                 <NewContactAdvice requests={userContacts} />
                 <NewMessageAdvice messages={unRead} />
-            </>
+            </div>
         }
-    </>);
+        <nav>
+            <NavLink to="/" className={({ isActive }) => (isActive ? 'active' : '')}>Inicio</NavLink>
+            <NavLink to="/dashboard" className={({ isActive }) => (isActive ? 'active' : '')}>Dashboard</NavLink>
+            <NavLink to="/profile" className={({ isActive }) => (isActive ? 'active' : '')}>Perfil</NavLink>
+            <NavLink to="/contacts" className={({ isActive }) => (isActive ? 'active' : '')}>Contactos</NavLink>
+            <NavLink to="/search" className={({ isActive }) => (isActive ? 'active' : '')}>Buscar</NavLink>
+            {logedIn ? (
+                <NavLink to="/logout" className={({ isActive }) => (isActive ? 'active' : '')}>DESCONTECTAR</NavLink>
+            ) : (
+                <NavLink to="/login" className={({ isActive }) => (isActive ? 'active' : '')}>CONECTAR</NavLink>
+            )}
+        </nav>
+    </div>);
 }
 
 export default NavBar;
