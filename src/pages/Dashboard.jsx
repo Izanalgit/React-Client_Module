1279463197a9686096be8f479a31f26141493594
Izanalgit <izanalvarez@gmail.com>
2024-12-08 17:49:09 +0100

    update : dashboard page

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,52 +1,80 @@
 import { useState } from "react";
 
 import { useApp } from "../context/AppContext";
+import wellcomeService from "../services/wellcomeService";
 import UserUpdateFrom from "../components/user/UserUpdateFrom";
 import UserDeleteForm from "../components/user/UserDeleteForm";
 import BlockUserList from "../components/privacy/BlockUserList";
 import PremyMenu from "../components/premy/PremyMenu";
 
+import '../css/Dashboard.css';
+
 const Dashboard = () => {
 
+    const [isBuying,setIsBuying] = useState(false);
     const [isEditingUser,setIsEditingUser] = useState(false);
     const [isDeletingUser,setIsDeletingUser] = useState(false);
     const [isManagingBlocks,setIsManagingBlocks] = useState(false);
     
 
-    const {logedIn,userPremy} = useApp();
+    const {logedIn,userPremy,userProfile} = useApp();
+
+    const wellcomeMessage = wellcomeService(userProfile?.userProfile?.genre) || '';
 
     const handleCompleteAction = () => {
         setIsEditingUser(false);
-        // setIsManagingBlocks(false);
+    };
+    const handleCompleteBuy = () => {
+        setIsBuying((prev)=>!prev);
     };
 
     return (
         
-        <>
+        <div className="dashboard-user">
             {!logedIn && <h5>No estás conectado!</h5>}
             {logedIn &&
             <>
-                <h2>DASHBORAD</h2>
-                {!isEditingUser && !isDeletingUser && !isManagingBlocks &&
-                    <p>Hola {logedIn}</p>
+                {!isBuying &&
+                    <div className="manage-buttons">
+                        <button 
+                            onClick={() => setIsEditingUser(!isEditingUser)} 
+                            disabled={isDeletingUser || isManagingBlocks}
+                        >
+                            {isEditingUser ? 'Cancelar' : 'Editar Usuario'}
+                        </button>
+                        <button 
+                            onClick={() => setIsDeletingUser(!isDeletingUser)}
+                            disabled={isEditingUser || isManagingBlocks}
+                        >
+                            {isDeletingUser ? 'Cancelar' : 'Eliminar Usuario'}
+                        </button>
+                        <button 
+                            onClick={() => setIsManagingBlocks(!isManagingBlocks)}
+                            disabled={isDeletingUser || isEditingUser}
+                        >
+                            {isManagingBlocks ? 'Cancelar' : 'Blockeados'}
+                        </button>
+                    </div>
                 }
-                {<PremyMenu premium={userPremy}/>}
-                {isEditingUser && <UserUpdateFrom onComplete={handleCompleteAction} />}
-                {isDeletingUser && <UserDeleteForm />}
-                {isManagingBlocks && <BlockUserList />}
-
-                <button onClick={() => setIsEditingUser(!isEditingUser)}>
-                    {isEditingUser ? 'Cancelar' : 'Editar Usuario'}
-                </button>
-                <button onClick={() => setIsDeletingUser(!isDeletingUser)}>
-                    {isDeletingUser ? 'Cancelar' : 'Eliminar Usuario'}
-                </button>
-                <button onClick={() => setIsManagingBlocks(!isManagingBlocks)}>
-                    {isManagingBlocks ? 'Cancelar' : 'Blockeados'}
-                </button>
+                <div className="dashboard-content">
+                    {!isEditingUser && !isDeletingUser && !isManagingBlocks &&
+                        <div>
+                            {!isBuying && 
+                            <>
+                                <h3>CONFIGURACIÓN DE USUARIO :</h3>
+                                <p>Hola {logedIn}. {wellcomeMessage}</p>
+                            </>
+                            }
+                            <PremyMenu premium={userPremy} onComplete={handleCompleteBuy}/>
+                        </div>
+                    }
+                    {isEditingUser && <UserUpdateFrom onComplete={handleCompleteAction} />}
+                    {isDeletingUser && <UserDeleteForm />}
+                    {isManagingBlocks && <BlockUserList />}
+                </div>
             </>
             }
-        </>
+        </div>
     )
     
 
