17a1206a57ba68d1697d0fe74d5c04c1aac0988b
Izanalgit <izanalvarez@gmail.com>
2024-12-10 13:35:40 +0100

    update : premy view on search profile card

--- src/components/search/SearchProfileCard.jsx
+++ src/components/search/SearchProfileCard.jsx
@@ -1,3 +1,8 @@
+import { useState , useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { useApp } from "../../context/AppContext";
+
 import ContactRequest from "../contacts/ContactRequest";
 import defaultProfile from '../../assets/images/profile-default.png';
 
@@ -5,14 +10,39 @@ import '../../css/SearchCard.css';
 
 const SearchProfileCard = ({ contactId, contactName, contactImage, contactBio}) => {
 
+    const navigate = useNavigate();
+    const {userContacts,userPremy} = useApp();
+
+    const [ isContact , setIsContact ] = useState(false);
+    
+    const contacts = userContacts?.contactsList?.contacts
+    const contact = contacts ? contacts.find((contact)=>contact.contactId == contactId) : null;
+
+    const premyTime = userPremy?.premiumTime || 0;
+
+    useEffect(()=>{
+        if(contact)
+            setIsContact(true);
+    },[contact])
+
     const profileImage = contactImage ? contactImage : defaultProfile;
 
     return (
         <div className="search-card">
+            {premyTime > 0 && !isContact &&
+                <div className="premy-profile-view">
+                <button 
+                    onClick={()=>{navigate(`/contact/${contactId}`)}}    
+                />
+                </div>
+            }
             <h4>{contactName}</h4>
             <img src={profileImage} alt="Profile" />
             <p>{contactBio}</p>
-            <ContactRequest contactId={contactId}/>
+            {isContact 
+                ? <button onClick={()=>{navigate(`/contact/${contactId}`)}}>Perfil</button>
+                : <ContactRequest contactId={contactId}/>
+            }
         </div>
     );
 };
