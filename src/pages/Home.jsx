a35c8b62f0bc19ef72dee0b34df3ae4f30bb75d0
Izanalgit <izanalvarez@gmail.com>
2024-12-11 21:12:58 +0100

    feat : for dev change theme button

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,24 @@
+import { useState, useEffect } from "react";
+
 const Home = () => {
+
+    const [theme, setTheme] = useState("light");
+
+    useEffect(() => {
+        document.documentElement.setAttribute("data-theme", theme);
+    }, [theme]);
+
+    const toggleTheme = () => {
+        setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+    };
+
     return (
         <>
             <h1>LANDING PAGE AQUÍ !</h1>
+
+            <button onClick={toggleTheme}>
+                Cambiar a {theme === "light" ? "Oscuro" : "Claro"}
+            </button>
         </>
     )
 };
